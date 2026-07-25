aeec54929b0e76f0a5f4fbbf5eac4768843b2ba8
jaasi <jaasi@JAASIEL>
2026-07-24 19:39:19 -0600

    hola

--- hola_mundo/src/hola_mundo/Hola_mundo.java
+++ hola_mundo/src/hola_mundo/Hola_mundo.java
@@ -14,7 +14,31 @@ public class Hola_mundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola...como estas?");
+        System.out.println("Este es mi primer trabajo");
+        System.out.println("Yujuuuu...estoy con java");
         // TODO code application logic here
+        System.out.printf("Esto es la clase de SS","Programacion"); System.out.println("|");
+System.out.printf("Esto es oS la clase de","Programacion"); System.out.println("");
+System.out.printf("%S Esto es la clase de","Programacion"); System.out.println("");
+System.out.printf("Esto es oS la clase de\n","Programacion"); //System.out.println("");
+System.out.printf("8S Esto es 1a clase de\n", "Programacion"); //System.out.println("");
+System.out.printf("Hola \n Como \n estas\n en la clase %s\n?","Programacion");
+System.out.printf("Hola \t Como \t estas\t en la clase %s\t?","Programacion");
+
+       //Variables
+        int tiempo; //Tipo Entero
+        double promedio; //Tipo Decimal
+        String nombre;//Tipo Cadena
+        char inicial; //Tipo Caracter
+        boolean estado; //Tipo Booleano
+        
+        tiempo=3;
+        nombre="Jaasiel";
+        inicial='A';        
+        System.out.printf("Tu nombre es: %s \n",nombre);
+        System.out.printf("%d veces a la semana recibes la clase\n",tiempo) ;
+        System.out.printf("Te llamare gc inicial",inicial);
     }
     
 }
